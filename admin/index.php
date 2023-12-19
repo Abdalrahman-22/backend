36f059aac7420ec15245530a40d43c6b8e2756e0
Abdalrahman-22 <www.abdalrahman95.3@gmail.com>
2023-12-19 11:59:00 +0200

    editing loging & admin pages

--- admin/index.php
+++ admin/index.php
@@ -1,35 +1,164 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="style/ff.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<title>normal page</title>
+  <title>Admin Page</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <style>
+    /* Custom CSS for the page background with linear gradient */
+    body {
+      background: linear-gradient(120deg, #2980b9, #8e44ad);
+      min-height: 100vh;
+      margin: 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      color: white;
+    }
+
+    .container {
+      text-align: center;
+      padding: 20px;
+      max-width: 80%; /* Adjusted max-width for responsiveness */
+    }
+
+    h1 {
+      font-size: 2.5em;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+
+    #clock {
+      font-size: 2.2em;
+      font-weight: bold;
+    }
+
+    #info {
+      font-size: 1.2em;
+      margin-bottom: 20px;
+    }
+
+    a {
+      color: white;
+      text-decoration: none;
+      font-size: 1em;
+      display: block;
+      margin-bottom: 10px;
+      padding: 10px;
+      border-radius: 5px;
+      width: 100%; /* Default width for all devices */
+    }
+
+    .btn {
+      margin: 0 auto;
+      padding: 10px;
+      border-radius: 28px;
+      display: block;
+      
+    }
+
+    /* Different background colors for buttons */
+    .btn1 {
+      background-color: #3498db; /* Blue */
+    }
+
+    .btn2 {
+      background-color: #e67e22; /* Orange */
+    }
+
+    .btn3 {
+      background-color: #2ecc71; /* Green */
+    }
+
+    .btn4 {
+      background-color: #e74c3c; /* Red */
+    }
+
+    /* Improve button style */
+    .custom-button:hover {
+      background-color: #2980b9;
+    }
+
+    /* Media query for smaller screens (phones) */
+    @media (max-width: 576px) {
+      a {
+        width: 100%; /* Set width to 100% for smaller screens (phones) */
+      }
+
+    }
+
+    /* Media query for larger screens (PCs) */
+    @media (min-width: 992px) {
+      .btn {
+        width: 30%; /* Set width to 30% for larger screens (PCs) */
+        display: block;
+        margin-bottom: 10px;
+
+      }
+         /* Different background colors for buttons */
+    .btn1 {
+      width: 30%;
+    }
+
+    .btn2 {
+      width: 27%;
+    }
+
+    .btn3 {
+      width: 23%;
+    }
+
+    .btn4 {
+      width: 18%;
+    }
+
+    }
+  </style>
 </head>
 
 <body>
-<?php
+  <div class="container mt-5">
+    <?php
+      session_start();
+      if ($_SESSION['login'] == false || !(in_array($_SESSION['role'], array('admin')))) {// the second condition is !$_SESSION['role']=="admin"
+        header("location: ../index.php?loginError=1");
+        exit;
+      }
+    ?>
 
-	session_start();
-	if($_SESSION['login']==false or !(in_array($_SESSION['role'],array('admin'))))// the second condition is !$_SESSION['role']=="admin"
-	{
-		header("location: ../index.php?loginError=1");
-		exit;
-	}	
+    <h1>This is Admin Page</h1>
+    <div id="info">
+      <div>Welcome: <?php echo $_SESSION['userName']?></div>
+      <div id="clock"></div>
+      <div>Temperature: <span id="temperature">25°C</span>, Humidity: <span id="humidity">50%</span></div>
+    </div>
 
-?>
-	<h1>this is Admin page</h1>
-	<h1> Welcome : <?php echo $_SESSION['userName']?></h1>
-	<a href="del.php" >Edit users</a>
-	<br>
-	<a href="create_user.php" >add user</a>
-	<br>
-	<a href="../logout.php">logout</a>
-	<br>
-	<a href="http://192.168.8.101/room1">room1</a>
-	<a href="http://192.168.8.101/room2">room2</a>
+    <a href="del.php" class="btn btn-primary custom-button btn1">Edit Users</a>
+    <a href="create_user.php" class="btn btn-secondary custom-button btn2">Add User</a>
+    <a href="http://192.168.8.101/room1" class="btn btn-info custom-button btn3">MY Room</a>
+    <a href="http://192.168.8.101/room1" class="btn btn-info custom-button btn3">Room 1</a>
+    <a href="http://192.168.8.101/room2" class="btn btn-info custom-button btn3">Room 2</a>
+    <a href="../logout.php" class="btn btn-danger custom-button btn4">Logout</a>
 
-</body>
-</html>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script>
+    // Update the clock every second
+    function updateClock() {
+      var now = new Date();
+      var hours = now.getHours().toString().padStart(2, '0');
+      var minutes = now.getMinutes().toString().padStart(2, '0');
+
+      document.getElementById('clock').innerText = hours + ':' + minutes;
+    }
+
+    // Update temperature and humidity (placeholders in this example)
+    function updateTempHumidity() {
+      document.getElementById('temperature').innerText = '25°C'; // Placeholder, replace with real data
+      document.getElementById('humidity').innerText="10%";
+	}
