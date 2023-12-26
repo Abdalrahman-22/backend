64766f6ac5761c459cccb6293540ca405a9ae1da
Abdalrahman-22 <www.abdalrahman95.3@gmail.com>
2023-12-26 17:30:09 +0200

    modify create_user

--- admin/create_user.php
+++ admin/create_user.php
@@ -1,16 +1,53 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Create Account</title>
-  <link href="style/ccs.css" rel="stylesheet" type="text/css">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<?php
+  <style>
+    body {
+      background: linear-gradient(120deg, #2980b9, #8e44ad);
+      min-height: 100vh;
+      margin: 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      color: white;
+    }
 
+    #createuser-section {
+      max-width: 400px;
+      width: 100%;
+      margin-top: 50px;
+      padding: 20px;
+      background: linear-gradient(120deg, #2980b9, #8e44ad);
+      border-radius: 10px;
+      box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+      transition: background 0.3s, box-shadow 0.3s;
+    }
+
+    #createuser-section:hover {
+      background: linear-gradient(120deg, #3498db, #9b59b6);
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5);
+    }
+
+    .login-button {
+      background-color: #e74c3c;
+      border: none;
+      transition: background-color 0.3s;
+    }
+
+    .login-button:hover {
+      background-color: #c0392b;
+    }
+  </style>
+</head>
+
+<?php
 if (!empty($_POST)) {
   require_once("../Class.data.php");
   require_once("../Class.Tools.php");
@@ -20,39 +57,44 @@ if (!empty($_POST)) {
   } else {
     Tools::printError("Unexpected result: " . var_export($result, true));
   }
-
 }
 ?>
 
 <body>
   <?php
-
   session_start();
   if ($_SESSION['login'] == false or !(in_array($_SESSION['role'], array('admin')))) {
     header("location: ../index.php?loginError=1");
     exit;
   }
-
   ?>
-  <h1>Create Account</h1>
-  <form method="POST" action="create_user.php" enctype="multipart/form-data">
-    <label>Username:</label>
-    <input type="text" name="username" required>
-    <label>Password:</label>
-    <input type="password" name="password" required>
-    <label>Email:</label>
-    <input type="email" name="email" required>
-    <label>Role:</label>
-    <select name="role" required>
-      <option value="normal">normal</option>
-      <option value="admin">Admin</option>
-    </select>
-    <input type="submit" value="Create Account">
-    <input type="hidden" name="source" value="admin">
-    <a href="index.php" class="have-account-link">back</a>
-
-  </form>
-
+  <div id="createuser-section" class="mx-auto">
+    <h1 class="text-center mb-4">Create Account</h1>
+    <form method="POST" action="create_user.php" enctype="multipart/form-data">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" class="form-control" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+      </div>
+      <div class="form-group">
+        <label for="role">Role:</label>
+        <select class="form-control" id="role" name="role" required>
+          <option value="normal">Normal</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-danger btn-block login-button">Create Account</button>
+      <input type="hidden" name="source" value="admin">
+      <a href="index.php" class="btn btn-light btn-block mt-2">Back</a>
+    </form>
+  </div>
 </body>
 
 </html>
