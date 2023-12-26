a79ea98fdd1a346da32a4e380f9c496bbec0709d
Abdalrahman-22 <www.abdalrahman95.3@gmail.com>
2023-12-26 18:09:41 +0200

    modify delete page

--- admin/del.php
+++ admin/del.php
@@ -1,65 +1,113 @@
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            background: linear-gradient(120deg, #2980b9, #8e44ad);
+            min-height: 100vh;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            color: white;
+        }
 
+        #container {
+            max-width: 100%;
+            padding: 10px;
+            background: linear-gradient(120deg, #2980b9, #8e44ad);
+            border-radius: 10px;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+            transition: background 0.3s, box-shadow 0.3s;
+            margin: 10px auto;
+        }
+
+        #container:hover {
+            background: linear-gradient(120deg, #3498db, #9b59b6);
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5);
+        }
+
+        .container table {
+            width: 100%;
+            margin-bottom: 20px;
+            color: white;
+        }
+
+        .container table th,
+        .container table td {
+            border: none;
+            padding: 12px;
+        }
+
+        .container table th {
+            background-color: #2c3e50;
+        }
+
+        .container table tr:hover {
+            background-color: rgba(255, 255, 255, 0.1);
+        }
+
+        .container table .btn {
+            padding: 6px 12px;
+            font-size: 12px;
+        }
+
+        .back-button {
+            margin-top: 5px;
+            text-align: left;
+        }
+    </style>
 </head>
 
 <body>
-	<?php
-
-	session_start();
-	if ($_SESSION['login'] == false or !(in_array($_SESSION['role'], array('admin')))) {
-		header("location: ../index.php?loginError=1");
-		exit;
-	}
-
-	?>
-	<a href="../logout.php">logout</a>
-	<a href="index.php">back</a>
-	<div class="container">
-		<?php
-
-		require_once('../Class.data.php');
-
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {// same as if ( !empty($_POST)){
-			data::delete($_POST['name']);
-		}
-
-		$query1 = data::getAll();
-
-		echo "<table class='table table-hover'>";
-		echo "<tr><th>name</th><th>email</th><th>password</th><th>role</th><th>actions</th><th>actions</th></tr>";
-		while ($row = $query1->fetch()) {
-
-			echo "<tr><td>{$row['name']}</td><td>{$row['email']}</td><td>***</td><td>{$row['role']}</td>";
-
-			echo "<td>";
-			?>
-			<form action="" method="post" onsubmit="return confirm('are you sure to delete')">
-				<input type="hidden" id='name' name='name' value="<?php echo $row['name']; ?>" />
-				<input type="submit" value="delete" />
-			</form>
-			<?php
-			echo "</td><td>" ?>
-			<form action="edit.php" method="post" >
-				<input type="hidden" id='name' name='name' value="<?php echo $row['name']; ?>" />
-				<input type="submit" value="edit" />
-			</form>
-			<?php
-
-
-			echo "</td></tr>";
-
-		}
-		echo "</table>"
-			?>
-	</div>
+    <div id="container">
+        <h1 class="text-center">Users Information</h1>
+        <?php
+        session_start();
+        if ($_SESSION['login'] == false or !(in_array($_SESSION['role'], array('admin')))) {
+            header("location: ../index.php?loginError=1");
+            exit;
+        }
+        ?>
+        <?php
+        require_once('../Class.data.php');
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            data::delete($_POST['name']);
+        }
+        $query1 = data::getAll();
+        echo "<div class='table-responsive'>";
+        echo "<table class='table table-hover'>";
+        echo "<thead><tr><th>Name</th><th>Email</th><th>Password</th><th>Role</th><th>Actions</th><th>Actions</th></tr></thead>";
+        while ($row = $query1->fetch()) {
+            echo "<tr><td>{$row['name']}</td><td>{$row['email']}</td><td>***</td><td>{$row['role']}</td>";
+            echo "<td>";
+        ?>
+            <form action="" method="post" onsubmit="return confirm('Are you sure to delete?')">
+                <input type="hidden" name='name' value="<?php echo $row['name']; ?>" />
+                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+            </form>
+        <?php
+            echo "</td><td>";
+        ?>
+            <form action="edit.php" method="post">
+                <input type="hidden" name='name' value="<?php echo $row['name']; ?>" />
+                <button type="submit" class="btn btn-warning btn-sm">Edit</button>
+            </form>
+        <?php
+            echo "</td></tr>";
+        }
+        echo "</table>";
+        echo "</div>";
+        ?>
+        <div class="back-button">
+            <a href="index.php" class="btn btn-light">Back</a>
+        </div>
+    </div>
 </body>
 
 </html>
